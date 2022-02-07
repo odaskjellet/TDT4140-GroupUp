d64cdcd0e58a32e71fa07d66f43ccfeff63c07e0
Magnus Svendsen <magnsven@stud.ntnu.no>
2022-02-07 11:26:47 +0100

    Created SQLite database

---
+++ server/src/db.js
@@ -0,0 +1,31 @@
+// server/src/db.js
+import Database from 'better-sqlite3';
+
+export class NumbersDB {
+  constructor(filename) {
+    this.db = new Database(filename,
+      { verbose: (msg) => console.log('[DB] ' + msg) });
+    this.stmt_create = this.db.prepare(
+      'CREATE TABLE IF NOT EXISTS numbers (number int)');
+    this.stmt_create.run();
+    
+    this.stmt_get = this.db.prepare(
+      'SELECT * FROM numbers');
+    this.stmt_insert = this.db.prepare(
+      'INSERT INTO numbers (number) VALUES (?)');
+    this.stmt_clear = this.db.prepare(
+      'DELETE FROM numbers');
+  }
+  
+  getNumbers() {
+    return this.stmt_get.all();
+  }
+  
+  insertNumber(number) {
+    this.stmt_insert.run(number);
+  }
+
+  clearNumbers() {
+    this.stmt_clear.run();
+  }
+}
--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,27 @@
+// server/src/index.js
 import express from 'express';
+import { NumbersDB } from './db.js';
 
 const PORT = 3001;
-const app = express();
+const server = express();
+const db = new NumbersDB(
+  process.argv[2] === 'test' ? ':memory:' : './data/database.db');
 
-app.listen(PORT, () => {
+server.get('/api/get', (request, result) => {
+  result.send(JSON.stringify(db.getNumbers()));
+});
+
+server.put('/api/insert', (request, result) => {
+  let number = Math.floor(Math.random() * 100);
+  db.insertNumber(number);
+  result.send();
+});
+
+server.delete('/api/clear', (request, result) => {
+  db.clearNumbers();
+  result.send();
+});
+
+server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
