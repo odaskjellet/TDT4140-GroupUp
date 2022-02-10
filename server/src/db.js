f5338fc4eb834ecc0ea40665d3b050706e4d5e8e
elifjoh <elifjoh@stud.ntnu.no>
2022-02-10 13:46:26 +0100

    Added Groups to the database #22 Co-authored-by: Oda Marie Imingen Skjellet <omskjell@ntnu.no>

--- server/src/db.js
+++ server/src/db.js
@@ -6,18 +6,30 @@ class Database {
   constructor(filename) {
     this.db = new SQLiteDB(filename,
       {verbose: (msg) => console.log('[DB] ' + msg)});
+
     this.stmt_create = this.db.prepare(
       'CREATE TABLE IF NOT EXISTS Users (username string, password string, age integer)');
     this.stmt_create.run();
+
+    this.stmt_create_group = this.db.prepare(
+      'CREATE TABLE IF NOT EXISTS Groups (id integer, name string)');
+      this.stmt_create_group.run();
 
     this.stmt_get = this.db.prepare(
       'SELECT (username) FROM Users');
     
     this.stmt_get_userinfo = this.db.prepare(
       'SELECT username, age FROM Users');
+    
+    this.stmt_get_group = this.db.prepare(
+      'SELECT id, name FROM Groups'
+    )
 
     this.stmt_insert = this.db.prepare(
       'INSERT INTO Users (username, password, age) VALUES (?, ?, ?)');
+
+    this.stmt_insert_group = this.db.prepare(
+      'INSERT INTO Groups (id, name) VALUES (?, ?)');
 
     this.stmt_try_login = this.db.prepare(
       'SELECT * FROM Users WHERE (username == ? AND password == ?)');
@@ -38,6 +50,15 @@ class Database {
   tryLogin(username, password) {
     return this.stmt_try_login.all(username, password).length > 0;
   }
+
+  insertGroup(id, name) {
+    return this.stmt_insert_group.run(id, name);
+  }
+
+  getGroups() {
+    return this.stmt_get_group.all();
+  }
+
 }
 
 module.exports = {Database};
