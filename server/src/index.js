8158856b0ce329407ced6f34022e973dc18943bb
Magnus Svendsen <magnsven@stud.ntnu.no>
2022-02-10 14:57:24 +0100

    Modified API to fit new DB-calls

--- server/src/index.js
+++ server/src/index.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const express = require('express');
 const {Database} = require('./db.js');
 
@@ -6,8 +7,14 @@ const server = express();
 const db = new Database(
   process.argv[2] === 'test' ? ':memory:' : './data/database.db');
 
+
+//TODO: API IS YET TO BE TESTED!
 server.get('/api/get', (request, result) => {
   result.send(JSON.stringify(db.getUsers()));
+});
+
+server.get('/api/get_userinfo', (request, result) => {
+  result.send(JSON.stringify(db.getUserInfo()));
 });
 
 server.put('/api/insert', (request, result) => {
@@ -21,7 +28,27 @@ server.put('/api/try_login', (request, result) => {
   result.send(
     db.tryLogin(request.params.username, request.params.password)
   );
-})
+});
+
+server.put('/api/insert_group', (request, result) => {
+  db.insertGroup(request.params.id, request.params.name);
+  result.send('OK');
+});
+
+server.get('/api/get_group', (request, result) => {
+  result.send(JSON.stringify(db.getGroups()));
+});
+
+server.put('/api/insert_groupUser_relation', (request, result) {
+  db.addUserToGroup(request.params.username, request.params.groupID);
+  result.send('OK');
+});
+
+server.get('/api/get_group_members', (request, result) => {
+  result.send(JSON.stringify(db.getGroupMembers(request.params.groupID)));
+});
+
+
 
 server.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
