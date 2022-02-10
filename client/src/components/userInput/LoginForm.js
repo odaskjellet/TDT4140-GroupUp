862b290c174064dda73b87fa7743847d73be5a76
Vegard <vnhenrik@stud.ntnu.no>
2022-02-10 13:10:52 +0100

    Removed Numbers.jsx and test. Added Login form (no logic yet), added some css styling

---
+++ client/src/components/userInput/LoginForm.js
@@ -0,0 +1,31 @@
+import Card from '../../ui/Card';
+import classes from './LoginForm.module.css';
+
+
+/**
+ * Returns a login form wrapped in custom card div.
+ * @return {JSX.Element}
+ * @constructor
+ */
+function LoginForm() {
+  return <Card>
+    <form className={classes.form}>
+      <div className={classes.control}>
+        <label htmlFor={'username'}>Username</label>
+        <input type={'text'} required id={'username'}/>
+      </div>
+      <div className={classes.control}>
+        <label htmlFor={'password'}>Password</label>
+        <input type={'password'} required id={'password'}/>
+      </div>
+      <div className={classes.actions}>
+        <button>Login</button>
+      </div>
+
+    </form>
+
+  </Card>;
+}
+
+
+export default LoginForm;
