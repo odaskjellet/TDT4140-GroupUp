7a541fccbf7e4463d518f0e60de4c5d9dfa6f563
Vegard <vnhenrik@stud.ntnu.no>
2022-02-14 11:59:27 +0100

    Added age and gender field to registration UI. Also added client side form validation with react-hook-form

--- client/src/components/userInput/RegisterForm.js
+++ client/src/components/userInput/RegisterForm.js
@@ -1,3 +1,4 @@
+import {useForm} from 'react-hook-form';
 import Card from '../../ui/Card';
 import classes from './LoginForm.module.css';
 
@@ -8,42 +9,60 @@ import classes from './LoginForm.module.css';
  * @constructor
  */
 function RegisterForm() {
-  return <Card>
-    <form className={classes.form}>
+  const {register, formState: {errors}, handleSubmit} = useForm();
+  const onSubmit = (data) => console.log(data);
 
-      <div className={classes.control}>
-        <label htmlFor={'name'}>Your name</label>
-        <input type={'text'} required id={'name'}/>
-      </div>
 
-      <div className={classes.control}>
-        <label htmlFor={'age'}>Your age</label>
-        <input type={'number'} required id={'age'}/>
-      </div>
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
+        <div className={classes.control}>
+          <label htmlFor={'name'}>Your name</label>
+          <input {...register('name', {required: true, maxLength: 40, pattern: /^[a-z ,.'-]+$/i})} />
+          {errors.name?.type === 'required' && 'Name is required'}
+        </div>
 
-      <div className={classes.control}>
-        <label htmlFor={'Email'}>Email</label>
-        <input type={'email'} required id={'Email'}/>
-      </div>
+        <div className={classes.control}>
+          <label htmlFor={'name'}>Your age</label>
+          <input type="number" {...register('age', {required: true, min: 18, max: 99})} />
+          {errors.age && 'You need to be between 18 and 99 years old.'}
+        </div>
 
-      <div className={classes.control}>
-        <label htmlFor={'password'}>Password</label>
-        <input type={'password'} required id={'password'} placeholder={'6 characters'}/>
-      </div>
+        <div className={classes.control}>
+          <label htmlFor={'name'}>Your gender</label>
+          <select required={true} {...register('gender')}>
+            <option value="female">Female</option>
+            <option value="male">Male</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
 
-      <div className={classes.control}>
-        <label htmlFor={'password'}>Re-enter password</label>
-        <input type={'password'} required id={'password'}/>
-      </div>
 
-      <div className={classes.actions}>
-        <button>Create account</button>
-      </div>
+        <div className={classes.control}>
+          <label htmlFor={'email'}>Your email</label>
+          <input type={'email'} {...register('email', {required: true, maxLength: 40})} />
+          {errors.email && 'Email is required'}
+        </div>
 
-    </form>
+        <div className={classes.control}>
+          <label htmlFor={'password'}>Your password</label>
+          <input type={'password'} {...register('password', {required: true})} />
+          {errors.password && 'Password is required'}
+        </div>
 
-  </Card>;
+        <div className={classes.control}>
+          <label htmlFor={'password'}>Repeat password</label>
+          <input type={'password'} {...register('passwordRepeat', {required: true})} />
+          {errors.passwordRepeat && 'Repeat your password'}
+        </div>
+
+        <div className={classes.actions}>
+          <button>Create account</button>
+        </div>
+
+      </form>
+    </Card>
+  );
 }
-
 
 export default RegisterForm;
