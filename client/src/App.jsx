2f6ba05df7869a1e1ac47d82e6700f5d6a9ba2e2
Vegard <vnhenrik@stud.ntnu.no>
2022-02-14 12:12:39 +0100

    Added client side validation with react hook forms. Also using browser route from react router handle refresh the pages content without requesting new site from server.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import LoginPage from './pages/LoginPage';
+import {Route, Routes} from 'react-router-dom';
 
 
 function App() {
   return (
-    <LoginPage/>
-
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginPage/>}/>
+      </Routes>
+    </div>
   );
 }
 export default App;
--- client/src/components/userInput/LoginForm.js
+++ client/src/components/userInput/LoginForm.js
@@ -1,31 +1,46 @@
-import Card from '../../ui/Card';
+import {useForm} from 'react-hook-form';
 import classes from './LoginForm.module.css';
-
+import Card from '../../ui/Card';
+import {Link} from 'react-router-dom';
 
 /**
- * Returns a login form wrapped in custom card div.
+ * Returns a login form with client side validation.
  * @return {JSX.Element}
  * @constructor
  */
 function LoginForm() {
-  return <Card>
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor={'username'}>Username</label>
-        <input type={'text'} required id={'username'}/>
-      </div>
-      <div className={classes.control}>
-        <label htmlFor={'password'}>Password</label>
-        <input type={'password'} required id={'password'}/>
-      </div>
-      <div className={classes.actions}>
-        <button>Login</button>
-      </div>
+  const {register, formState: {errors}, handleSubmit} = useForm();
+  const onSubmit = (data) => console.log(data);
 
-    </form>
 
-  </Card>;
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
+        <div className={classes.control}>
+          <label htmlFor={'username'}>Username</label>
+          <input {...register('username', {required: true, maxLength: 40, pattern: /^[a-z ,.'-]+$/i})} />
+          {errors.username?.type === 'required' && 'Username is required'}
+        </div>
+
+
+        <div className={classes.control}>
+          <label htmlFor={'password'}>Your password</label>
+          <input type={'password'} {...register('password',
+              {required: true})} />
+          {errors.password && 'Password is required'}
+        </div>
+
+        <div className={classes.actions}>
+          <button>Login</button>
+          <Link to={'/register'}>
+            <button>Register</button>
+          </Link>
+        </div>
+
+
+      </form>
+    </Card>
+  );
 }
-
 
 export default LoginForm;
