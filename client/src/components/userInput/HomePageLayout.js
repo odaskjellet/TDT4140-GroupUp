f3bef79d2527c43bdd188fe3e7b40ce2f47b4c22
Magnus Svendsen <magnsven@stud.ntnu.no>
2022-02-17 12:33:20 +0100

    Added column layout

--- client/src/components/userInput/HomePageLayout.js
+++ client/src/components/userInput/HomePageLayout.js
@@ -6,8 +6,19 @@ function HomePageLayout() {
 
     return (
         <Card>
-            <div className={classes.profile}>
-                <h1>Hola!</h1>
+            <div className={classes.row}>
+                <div className={classes.profile}>
+                    <h2>Column 1</h2>
+                    <h4>Information about profile etc!</h4>
+                </div>
+                <div className={classes.information}>
+                    <h2>Column 2</h2>
+                    <h4>Informatin about happenings, matches etc?</h4>
+                </div>
+                <div className={classes.groups}>
+                    <h2>Column 3</h2>
+                    <h4>Information about my groups etc</h4>
+                </div>
             </div>
         </Card>
     )
