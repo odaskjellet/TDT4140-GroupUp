e27b9b1688f4bd2ad2037f3d0ab8168bf81d7363
elifjoh <elifjoh@stud.ntnu.no>
2022-02-28 19:58:26 +0100

    Added  apii tests for try-login and insert-group Issue #37

--- client/cypress/integration/api.test.js
+++ client/cypress/integration/api.test.js
@@ -83,4 +83,79 @@ describe('api', () => {
     const result = await fetch('/api/insert-user', requestOptions);
     expect(result.status).to.equal(400);
   });
+
+  it('should be able to log in', async () => {
+    const requestOptions = {
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        username: 'henrik',
+        password: 'henrik123',
+      }),
+    };
+    const result = await fetch('/api/try-login', requestOptions);
+    expect(result.statusText).to.equal('OK');
+  });
+
+  it('should not be able to log in with wrong password', async () => {
+    const requestOptions = {
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        username: 'henrik',
+        password: 'wrongPassword',
+      }),
+    };
+    const result = await fetch('/api/try-login', requestOptions);
+    expect(result.status).to.equal(400);
+  });
+
+  it('should not be able to log in with wrong username', async () => {
+    const requestOptions = {
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        username: 'henrikk',
+        password: 'henrik123',
+      }),
+    };
+    const result = await fetch('/api/try-login', requestOptions);
+    expect(result.status).to.equal(400);
+  });
+
+  it('should be able to insert group', async () => {
+    const requestOptions = {
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        name: 'groupA',
+        admin: 'henrik',
+        groupId: 1,
+      }),
+      };
+      await fetch('/api/insert-group', requestOptions);
+      const result = await fetch('/api/get-groups', {method: 'GET'})
+      .then((res) => res.json());
+      expect(result).to.deep.equal([{groupId: 1, name: 'groupA'}])
+  });
+
+  it('should not be able to insert group with invalid groupname', async () => {
+    const requestedOptions = {
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        name: 'group1',
+        admin: 'henrik',
+        groupId: 2,
+      }),
+    };
+    const result = await fetch('/api/insert-group', requestedOptions);
+    expect(result.status).to.equal(400);
+  });
+
+
+
+
 });
+
+
