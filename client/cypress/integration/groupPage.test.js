4dabfa5bdb55b28def1ec2be55de0498ac68126e
Vegard <vnhenrik@stud.ntnu.no>
2022-03-17 12:32:43 +0100

    feilsØKE

--- client/cypress/integration/groupPage.test.js
+++ client/cypress/integration/groupPage.test.js
@@ -1,71 +1,96 @@
-import { cyan } from "@mui/material/colors";
+import {cyan} from "@mui/material/colors";
 
-describe('Pop up match info'), () => {
+describe('Pop up match info', () => {
 
     it('should start empty, but with a user', async () => {
         await fetch('/api/debug/clear', {method: 'DELETE'});
         const result = await fetch('/api/get-users', {method: 'GET'})
             .then((res) => res.json());
-        
+
         expect(result).to.be.empty;
-    
+
         await fetch('/api/insert-user', {
-          method: 'PUT',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            username: 'myUsername',
-            password: 'password123',
-            age: '20',
-            email: 'user@mail.com',
-          }),
-        });
-        
-        await fetch('/api/insert-group', {
-          method: 'PUT',
-          headers: {'Content-Type': 'application/json'},
-          body: JSON.stringify({
-            name: 'groupA',
-            admin: 'henrik',
-            groupId: 'g1',
-            description: 'En fin gruppe.',
-            location: 'Oslo',
-            membership: 'standard',
-            image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Gull_portrait_ca_usa.jpg/1280px-Gull_portrait_ca_usa.jpg',
-            interests: ['Fishing', 'Golf'],
-          }),
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                username: 'myUsername',
+                password: 'password123',
+                age: '20',
+                email: 'user@mail.com',
+            }),
         });
 
         await fetch('/api/insert-group', {
             method: 'PUT',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
-              name: 'groupB',
-              admin: 'henrik',
-              groupId: 'g1',
-              description: 'En fin gruppe.',
-              location: 'Oslo',
-              membership: 'standard',
-              image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Gull_portrait_ca_usa.jpg/1280px-Gull_portrait_ca_usa.jpg',
-              interests: ['Fishing', 'Golf'],
+                name: 'groupA',
+                admin: 'henrik',
+                groupId: 'g1',
+                description: 'En fin gruppe.',
+                location: 'Oslo',
+                membership: 'standard',
+                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Gull_portrait_ca_usa.jpg/1280px-Gull_portrait_ca_usa.jpg',
+                interests: ['Fishing', 'Golf'],
             }),
-          });
+        });
 
-        
-    await fetch('/api/match-groups', {
-      method: 'PUT',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        primaryId: selectedGroupA.groupId,
-        secondaryId: selectedGroupBId,
-      }),
-    }).then((res) => {
-      if (res.ok) {
-        setSnackbarOpen(true);
-      } else {
-        // Did not create match
-      }
-    });
-    });
+        await fetch('/api/insert-group', {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                name: 'groupB',
+                admin: 'henrik',
+                groupId: 'g2',
+                description: 'En fin gruppe.',
+                location: 'Oslo',
+                membership: 'standard',
+                image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Gull_portrait_ca_usa.jpg/1280px-Gull_portrait_ca_usa.jpg',
+                interests: ['Fishing', 'Golf'],
+            }),
+        });
 
-    cy.visit('home');
-}
+
+        fetch('/api/match-groups', {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                primaryId: 'g2',
+                secondaryId: 'g1',
+            }),
+        }).then((res) => {
+            if (res.ok) {
+                console.log("ok");
+            } else {
+                // Did not create match
+            }
+        });
+
+        fetch('/api/match-groups', {
+            method: 'PUT',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                primaryId: 'g1',
+                secondaryId: 'g2',
+            }),
+        }).then((res) => {
+            if (res.ok) {
+                console.log("ok");
+            } else {
+                // Did not create match
+            }
+        });
+
+        it('existing values should appear', () => {
+            cy.visit('group/g1');
+            cy.contains(/Visit/i).click();
+
+            cy.contains(/Admin: henrik/i)
+            cy.contains(/Location: Oslo/i)
+            cy.contains(/Description: En fin gruppe./i)
+
+
+        })
+
+
+    })}
