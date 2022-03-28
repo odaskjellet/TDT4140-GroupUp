d6ac05c6348e9a3dd860a7169afca3043fc3d960
magnetenstad <tenstad.magne@gmail.com>
2022-03-28 12:43:46 +0200

    Remove debug logs

--- server/src/index.js
+++ server/src/index.js
@@ -54,7 +54,6 @@ server.put('/api/try-login', (request, result) => {
 });
 
 server.put('/api/insert-group', (request, result) => {
-  console.log('\n\n\nBODY', request.body, '\n\n\n');
   if (validGroupName(request.body.name)) {
     db.insertGroup(request.body.groupId, request.body.name,
         request.body.admin, request.body.description, request.body.membership,
@@ -346,7 +345,6 @@ function clearErrors(registrationErrors) {
  * @return {*} a new array
  */
 function filterGroups(data) {
-  console.log(data);
   const arrays = [db.getAllGroups()];
 
   if (data.interests !== undefined) {
